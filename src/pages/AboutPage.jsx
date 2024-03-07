fa5b0b7b02b16469be14663159e0d53f6d2b60ce
Natalie <rivero_natalie@outlook.com>
2024-03-06 19:05:33 -0500

    added stuff to error page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,29 +6,43 @@ import React from "react";
 function AboutPage() {
   return (
     <React.Fragment>
-      <h1 className="p-4 mt-20 text-3xl font-bold text-blue-300">
+      <h1 className="p-4 mt-20 text-3xl font-bold text-blue-700 flex justify-center">
         {" "}
         About Rick N Morty:{" "}
       </h1>
-      <h2 className="p-4 mt-20 bg-blue-200 bg-opacity-50">
+      <h2 className="p-4 mt-20 bg-blue-200 bg-opacity-50 text-2xl border border-rounded m-16">
         {" "}
-        "Rick and Morty" is an American animated science fiction sitcom created
-        by Justin Roiland and Dan Harmon. The show premiered on December 2,
-        2013, on Adult Swim, a nighttime programming block on Cartoon Network.
-        The series follows the misadventures of an eccentric and alcoholic
-        scientist named Rick Sanchez and his good-hearted but easily influenced
-        grandson, Morty Smith. Together, they embark on various adventures
-        across different dimensions, planets, and timelines, often encountering
-        bizarre and fantastical creatures, as well as dealing with complex and
-        philosophical themes. The show is known for its dark humor, satirical
-        take on science fiction tropes, and exploration of existential themes
-        such as the meaning of life, free will, and the nature of reality. It
-        has received critical acclaim for its clever writing, imaginative
-        storytelling, and inventive use of science fiction concepts. "Rick and
-        Morty" has gained a dedicated fanbase and has become a cultural
-        phenomenon since its debut, spawning merchandise, video games, and a
-        passionate online community. It has also won several awards, including a
-        Primetime Emmy Award for Outstanding Animated Program.
+        <p>
+          "Rick and Morty" is an American animated science fiction sitcom
+          created by Justin Roiland and Dan Harmon. The show premiered on
+          December 2, 2013, on Adult Swim, a nighttime programming block on
+          Cartoon Network.
+        </p>
+        <br />
+        <p>
+          The series follows the misadventures of an eccentric and alcoholic
+          scientist named Rick Sanchez and his good-hearted but easily
+          influenced grandson, Morty Smith. Together, they embark on various
+          adventures across different dimensions, planets, and timelines, often
+          encountering bizarre and fantastical creatures, as well as dealing
+          with complex and philosophical themes.
+        </p>{" "}
+        <br />
+        <p>
+          The show is known for its dark humor, satirical take on science
+          fiction tropes, and exploration of existential themes such as the
+          meaning of life, free will, and the nature of reality. It has received
+          critical acclaim for its clever writing, imaginative storytelling, and
+          inventive use of science fiction concepts.
+        </p>{" "}
+        <br />
+        <p>
+          "Rick and Morty" has gained a dedicated fanbase and has become a
+          cultural phenomenon since its debut, spawning merchandise, video
+          games, and a passionate online community. It has also won several
+          awards, including a Primetime Emmy Award for Outstanding Animated
+          Program.
+        </p>
       </h2>
     </React.Fragment>
   );
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,8 +2,17 @@ import React from "react";
 function NotFound() {
   return (
     <div>
-      <h1>404 - Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
+      <h1 className="text-4xl flex justify-center m-20 text-green-400 font-bold">
+        404 - Not Found
+      </h1>
+      <br />
+      <p className="text-4xl flex flex-col justify-items-center m-10 text-green-400 font-bold">
+        Sorry, the page you are looking for does not exist.
+        <img
+          className="m-10 basis 1/2"
+          src="https://blog.lootcrate.com/wp-content/uploads/2018/08/rm_gc_9.gif"
+        />
+      </p>
     </div>
   );
 }
